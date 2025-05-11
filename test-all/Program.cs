aec5b93292c95042ff6d7dc9ae9f645a7ecc91e9
zenuas@gmail.com <zenuas@gmail.com>
2025-05-12 00:36:22 +0900

    dynamic型と具体的な方の記述方法確認

--- test-all/Program.cs
+++ test-all/Program.cs
@@ -1,1 +1,27 @@
-﻿Sate.TestAll.TestDir.Dummy.Build(new { Id = 1 });
+﻿using System;
+
+Sate.TestAll.TestDir.Dummy.Build(new { Id = 1 });
+
+TestMethod.Test(new { Id = 1, Name = "ab" });
+TestMethod.Test(new() { Id = 2, Name = "cd" });
+
+public class TestMethod
+{
+    public static void Test(dynamic a)
+    {
+        Test(new TestClass { Id = a.Id, Name = a.Name });
+    }
+
+    public static void Test(TestClass a)
+    {
+        Console.WriteLine(a.Id);
+        Console.WriteLine(a.Name);
+    }
+}
+
+
+public class TestClass
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
