5a8e5659549def54c1f04224e033c2b72f402ded
zenuas@gmail.com <zenuas@gmail.com>
2025-05-15 01:14:52 +0900

    ベンチマーク追加

---
+++ bench/GeneratorBench.cs
@@ -0,0 +1,60 @@
+﻿using BenchmarkDotNet.Attributes;
+using RazorEngine;
+using RazorEngine.Templating;
+
+namespace Sate.Benchmark;
+
+public class GeneratorBench
+{
+    public GeneratorBench()
+    {
+        Engine.Razor.Compile(RazorSource, "RazorSource", typeof(Model));
+    }
+
+    [Benchmark(Description = "Sate(dynamic)")]
+    public void SateBench()
+    {
+        _ = SateTest.Build(new { Name = "John Does", Age = 10 });
+    }
+
+    [Benchmark(Description = "Razor(Run)")]
+    public void RazorRunBench()
+    {
+        _ = Engine.Razor.Run("RazorSource", typeof(Model), new Model { Name = "John Does", Age = 10 });
+    }
+
+    [Benchmark(Description = "Razor(RunCompile)")]
+    public void RazorRunCompileBench()
+    {
+        _ = Engine.Razor.RunCompile(RazorSource, "templateKey", typeof(Model), new Model { Name = "John Does", Age = 10 });
+    }
+
+    public string RazorSource { get; } = @"
+/*
+    SQL文サンプル
+*/
+select *
+from Dummy
+where 1 = 1
+
+@if (Model.Name != """")
+{
+@: and   Name = @@Name
+}
+
+@if (Model.Age >= 20)
+{
+@: and   Age = @@Age
+}
+
+order by
+    Id,
+    Name
+";
+
+    public class Model
+    {
+        public required string Name { get; init; }
+        public required int Age { get; init; }
+    }
+}
---
+++ bench/SateTest.ts.sql
@@ -0,0 +1,18 @@
+﻿/*
+	SQL文サンプル
+*/
+select *
+from Dummy
+where 1 = 1
+
+/* IF @Name != '' */
+and   Name = @Name
+/* END */
+
+/* IF @Age >= 20 */
+and   Age = @Age
+/* END */
+
+order by
+	Id,
+	Name
